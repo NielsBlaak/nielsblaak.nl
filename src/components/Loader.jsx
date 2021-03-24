12a727c70cafc847b90e9e981a64539f9795b820
NielsBlaak <nielsblaak@hotmail.com>
2021-03-24 20:34:07 +0100

    Updates

--- src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../scss/loader.scss';
-
-const Loader = () => (
-    <div className="loader" />
-);
-
-export default React.memo(Loader);
--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Timeline from '../components/Timeline';
-
-const Home = () => (
-    <Timeline />
-)
-
-export default Home;
