a2e97763b1c998e8790acfdaad928fc700b9f504
NielsBlaak <Nielsblaak@hotmail.com>
2021-03-24 18:39:09 +0100

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,11 @@
 import Home from './pages/Home';
+import TussenStand from './pages/TussenStand';
 
 const routes = [{
+    path: '/fifa',
+    component: TussenStand,
+    exact: true,
+}, {
     path: '/',
     component: Home,
     exact: true,
